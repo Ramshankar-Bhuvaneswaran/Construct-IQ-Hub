d50c4a5dbc5c1b602edf9b15451c5f29579fb021
Ramshankar-Bhuvaneswaran <bhuvaneshwaran.r@northeastern.edu>
2024-04-13 17:25:09 -0400

    Basic-Project-Creation-2

---
+++ Construct/src/construct/Configure.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package construct;
+
+/**
+ *
+ * @author Ram
+ */
+public class Configure {
+    //Main class to configure the construction software
+    
+}
