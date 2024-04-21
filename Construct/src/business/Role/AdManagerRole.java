a53f4280f0b7872527bd7cac19522e0d52aa0254
SAI SRUNITH SILVERY <silvery.s@northeastern.edu>
2024-04-21 16:45:12 -0400

    commiting changes

---
+++ Construct/src/business/Role/AdManagerRole.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Role;
+
+//import UI.Advertisment.AdCompany;
+//import UI.Advertisment.AdManage;
+import UI.Advertisment.AdManage;
+import business.Business;
+import business.Organization.AdvertisingOrganization;
+import business.Organization.SupplierOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import ui.DoctorRole.DoctorWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class AdManagerRole extends Role {
+
+    public AdManagerRole() {
+        this.type = RoleType.AdManager;
+    }
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Business business) {
+        this.type = RoleType.AdManager;
+        return new AdManage(userProcessContainer, account, (AdvertisingOrganization) organization, business);
+    }
+
+}
