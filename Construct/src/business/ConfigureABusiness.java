bd9138b57042c9c36c0f22f103193eb021101179
Ramshankar-Bhuvaneswaran <bhuvaneshwaran.r@northeastern.edu>
2024-04-20 20:27:04 -0400

    Redefining-the-internal-structure

--- Construct/src/business/ConfigureABusiness.java
+++ Construct/src/business/ConfigureABusiness.java
@@ -12,17 +12,29 @@ import Logistics.BookingList;
 import Logistics.RentalCompany;
 import Logistics.RentalCompanyList;
 import Logistics.Vehicle;
+import Supplier.Product;
+import Supplier.ProductCatalog;
+import Supplier.Supplier;
+import Supplier.SupplierDirectory;
 import business.Employee.Employee;
 import business.Organization.AdminOrganization;
 import business.Organization.AdvertisingOrganization;
 import business.Organization.LogisticsOrganization;
 import business.Role.AdCompanyRole;
 import business.Role.AdManagerRole;
+import business.Organization.QAOrganization;
+import business.Organization.SupplierOrganization;
 import business.Role.AdminRole;
 import business.Role.LogisticsManagerRole;
 import business.Role.RentalCompanyRole;
+import business.Role.CivilEngineer;
 import business.UserAccount.UserAccount;
+import java.awt.Component;
 import java.util.Date;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 
 /**
  *
@@ -98,6 +110,12 @@ public class ConfigureABusiness {
         account6.setRole(new AdCompanyRole()); // Assuming the same role as AdCompany
         account6.setEmployee(employee5);
 
+        UserAccount account1 = new UserAccount();
+        account1.setUsername("civil");
+        account1.setPassword("civil");
+        account1.setRole(new CivilEngineer());
+        account1.setEmployee(employee);
+        
         adminOrganization.getEmployeeDirectory().getEmployeeList().add(employee);
         adminOrganization.getUserAccountDirectory().getUserAccountList().add(account);
         
@@ -121,6 +139,15 @@ public class ConfigureABusiness {
          blist.addVehicle(rentalCompany, null);
         }}
         Vehicle vehicle = new Vehicle("VH101", "Truck", 2.5, 75.0);
+
+        SupplierOrganization supprg = new SupplierOrganization();
+        business.getOrganizationDirectory().getOrganizationList().add(supprg);
+        
+        QAOrganization qa=new QAOrganization();
+        business.getOrganizationDirectory().getOrganizationList().add(qa);
+        
+        Vehicle vehicle = new Vehicle("VH101", "Truck", 2.5, 20.0, 75.0);
+
         
         RentalCompany rentalCompany = new RentalCompany("Best Rentals");
 //        rentalCompany.addVehicle(vehicle);
@@ -164,7 +191,30 @@ public class ConfigureABusiness {
 //        partner.displayDetails();
 
         
+       SupplierDirectory sd= supprg.getSuppliers();
+       Supplier s=sd.newSupplier("Hardware");
+       ProductCatalog pc=s.getPc();
+       ImageIcon image= new ImageIcon(ConfigureABusiness.class.getResource("/images/building.jpg"));
+       Product p=pc.newProduct("Steel claw hammer",25,"16 oz weight, comfortable rubber grip, durable and perfect for general carpentry" , 500, image);
+       
+       
+        
+        
         return business;
+        
+        
+    }
+    public class ImageRenderer extends DefaultTableCellRenderer {
+ 
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            if (value instanceof ImageIcon) {
+                JLabel label = new JLabel();
+                label.setIcon((ImageIcon) value);
+                return label;
+            }
+            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        }
     }
     
 }
